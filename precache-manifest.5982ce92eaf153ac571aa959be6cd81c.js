1794e0a12f15876c35e4b6904e6cc91271727083
mlmar <marcloussm@gmail.com>
2021-04-01 19:00:48 -0700

    Updates

---
+++ precache-manifest.5982ce92eaf153ac571aa959be6cd81c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e859bf0d6770add5c124c7011a48e730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b51be00bf7e7fd92f129",
+    "url": "/static/css/main.09b9f99b.chunk.css"
+  },
+  {
+    "revision": "906407fafcd9e1585b8d",
+    "url": "/static/js/2.71ce4721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71ce4721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51be00bf7e7fd92f129",
+    "url": "/static/js/main.ad7d1387.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "c4bd9291a65811a12a7d17f80abd592c",
+    "url": "/static/media/Mail.c4bd9291.png"
+  },
+  {
+    "revision": "07f11f65e75d49ee93f9a952ffca6e8a",
+    "url": "/static/media/block-screen.07f11f65.gif"
+  },
+  {
+    "revision": "1258be078c612472ffeaee2bba104cfe",
+    "url": "/static/media/nunito-sans-v6-latin-800.1258be07.ttf"
+  },
+  {
+    "revision": "67bf6e8048b65d32c0f8618a378b9beb",
+    "url": "/static/media/nunito-sans-v6-latin-800.67bf6e80.svg"
+  },
+  {
+    "revision": "6a4c29ad18bd8072a9a9d2443d0e340e",
+    "url": "/static/media/nunito-sans-v6-latin-800.6a4c29ad.woff2"
+  },
+  {
+    "revision": "a28845a6196298dad6dc0b2ba0b6de55",
+    "url": "/static/media/nunito-sans-v6-latin-800.a28845a6.woff"
+  },
+  {
+    "revision": "f78c45ac4cd947dc815e5c1150deb0e1",
+    "url": "/static/media/nunito-sans-v6-latin-800.f78c45ac.eot"
+  },
+  {
+    "revision": "1321ef46f9cc5d9b17568c20dc3106bf",
+    "url": "/static/media/nunito-sans-v6-latin-regular.1321ef46.eot"
+  },
+  {
+    "revision": "49ffe17ecbcd39ee72e4646616db3d80",
+    "url": "/static/media/nunito-sans-v6-latin-regular.49ffe17e.ttf"
+  },
+  {
+    "revision": "8a4e78bddeb15247220a5dcc522c6ae5",
+    "url": "/static/media/nunito-sans-v6-latin-regular.8a4e78bd.svg"
+  },
+  {
+    "revision": "8ace450709844437a579d4f4c7c37b42",
+    "url": "/static/media/nunito-sans-v6-latin-regular.8ace4507.woff"
+  },
+  {
+    "revision": "9c6a1ff29aaba0c21496eac04be062df",
+    "url": "/static/media/nunito-sans-v6-latin-regular.9c6a1ff2.woff2"
+  },
+  {
+    "revision": "14bbab96435fbf2a550966d3f5be1891",
+    "url": "/static/media/tonedeaf-screen.14bbab96.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mlmar.github.io/precache-manifest.90fa312f23d7f4119494971382e3c556.js"
+  "/precache-manifest.5982ce92eaf153ac571aa959be6cd81c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mlmar.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
