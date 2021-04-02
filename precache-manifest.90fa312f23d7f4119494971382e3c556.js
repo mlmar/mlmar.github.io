8c3d974534c6ffccaaf7901e016d6ebd6ff65535
mlmar <marcloussm@gmail.com>
2021-04-01 18:55:14 -0700

    Updates

---
+++ precache-manifest.90fa312f23d7f4119494971382e3c556.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762ebd2e3eb765863af6eb1300810afc",
+    "url": "/mlmar.github.io/index.html"
+  },
+  {
+    "revision": "8b707f97d69d1ec437a6",
+    "url": "/mlmar.github.io/static/css/main.9b0ac3d7.chunk.css"
+  },
+  {
+    "revision": "906407fafcd9e1585b8d",
+    "url": "/mlmar.github.io/static/js/2.71ce4721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mlmar.github.io/static/js/2.71ce4721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b707f97d69d1ec437a6",
+    "url": "/mlmar.github.io/static/js/main.750792ed.chunk.js"
+  },
+  {
+    "revision": "62435d08bbbbeb666e34",
+    "url": "/mlmar.github.io/static/js/runtime-main.5c935958.js"
+  },
+  {
+    "revision": "c4bd9291a65811a12a7d17f80abd592c",
+    "url": "/mlmar.github.io/static/media/Mail.c4bd9291.png"
+  },
+  {
+    "revision": "f66f765a874f82834ef5f92553df33ea",
+    "url": "/mlmar.github.io/static/media/block-mobile.f66f765a.gif"
+  },
+  {
+    "revision": "07f11f65e75d49ee93f9a952ffca6e8a",
+    "url": "/mlmar.github.io/static/media/block-screen.07f11f65.gif"
+  },
+  {
+    "revision": "1258be078c612472ffeaee2bba104cfe",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-800.1258be07.ttf"
+  },
+  {
+    "revision": "67bf6e8048b65d32c0f8618a378b9beb",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-800.67bf6e80.svg"
+  },
+  {
+    "revision": "6a4c29ad18bd8072a9a9d2443d0e340e",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-800.6a4c29ad.woff2"
+  },
+  {
+    "revision": "a28845a6196298dad6dc0b2ba0b6de55",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-800.a28845a6.woff"
+  },
+  {
+    "revision": "f78c45ac4cd947dc815e5c1150deb0e1",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-800.f78c45ac.eot"
+  },
+  {
+    "revision": "1321ef46f9cc5d9b17568c20dc3106bf",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-regular.1321ef46.eot"
+  },
+  {
+    "revision": "49ffe17ecbcd39ee72e4646616db3d80",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-regular.49ffe17e.ttf"
+  },
+  {
+    "revision": "8a4e78bddeb15247220a5dcc522c6ae5",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-regular.8a4e78bd.svg"
+  },
+  {
+    "revision": "8ace450709844437a579d4f4c7c37b42",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-regular.8ace4507.woff"
+  },
+  {
+    "revision": "9c6a1ff29aaba0c21496eac04be062df",
+    "url": "/mlmar.github.io/static/media/nunito-sans-v6-latin-regular.9c6a1ff2.woff2"
+  },
+  {
+    "revision": "14bbab96435fbf2a550966d3f5be1891",
+    "url": "/mlmar.github.io/static/media/tonedeaf-screen.14bbab96.png"
+  }
+]);
