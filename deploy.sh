edba0239f910ecca3c94e703162ca589d987496e
mlmar <marcloussm@gmail.com>
2021-12-20 00:37:17 -0800

    adding dist

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:mlmar/mlmar.github.io.git main
+git push -f git@github.com:mlmar/mlmar.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:mlmar/mlmar.git main:gh-pages
