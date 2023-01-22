e40af6687a5884a727143855312fb2f4e53b77a6
mlmar <marcloussm@gmail.com>
2023-01-21 17:15:12 -0800

    Removed old project links

---
+++ src/data/Projects.js
@@ -0,0 +1,22 @@
+/**
+ *  ARIBTRARY FILE FOR KEEPING AROUND OLD PROJECT JSON
+ */
+
+[
+  {
+    "title"       : "Block",
+    "description" : "Avoid being crushed by blocks in this simple game",
+    "github"      : "https://github.com/mlmar/Block",
+    "uri"         : "https://mlmar.github.io/Block/",
+    "imgPath"     : "images/block.gif",
+    "stack"       : ["JavaScript"]
+  },
+  {
+    "title"       : "Block Multiplayer",
+    "description" : "Create a lobby and invite your friends by just sending them the room code or link",
+    "github"      : "https://github.com/mlmar/block-2",
+    "uri"         : "https://block10.herokuapp.com/",
+    "imgUri"      : "https://user-images.githubusercontent.com/63682846/146718954-48abf51e-9102-413e-89b9-57dc3266c7b5.png",
+    "stack"       : ["JavaScript", "Socket.IO"]
+  }
+]
