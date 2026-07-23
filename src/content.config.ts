0fd970f33347dcc49aa86509acff3a733b9914c3
mlmar <marcloussm@gmail.com>
2026-07-22 22:10:28 -0700

    Initial set up for new astro project

---
+++ src/content.config.ts
@@ -0,0 +1,31 @@
+import { defineCollection } from "astro:content";
+import { file } from "astro/loaders";
+import { z } from "astro/zod";
+
+const history = defineCollection({
+    loader: file('./src/config/history.json'),
+    schema: z.object({
+        start: z.string(),
+        end: z.string(),
+        title: z.string(),
+        product: z.string(),
+        company: z.string()
+    })
+});
+
+const projects = defineCollection({
+    loader: file('./src/config/projects.json'),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        github: z.string(),
+        uri: z.string(),
+        imgUri: z.string(),
+        stack: z.array(z.string()),
+    })
+})
+
+export const collections = {
+    history,
+    projects
+}
