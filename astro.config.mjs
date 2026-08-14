6439aa5569902f8487bf39d30da0a203fb47b9a7
mlmar <marcloussm@gmail.com>
2026-08-14 15:43:01 -0700

    Update font family and image dimensions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 3000,
     },
+    fonts: [
+        {
+            name: 'DM Sans',
+            cssVariable: '--font-dm-sans',
+            provider: fontProviders.google(),
+            weights: ['100 900'],
+            styles: ['normal'],
+            fallbacks: ['sans-serif']
+        }
+    ]
 });
