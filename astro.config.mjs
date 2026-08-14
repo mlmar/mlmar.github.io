1c3dd011854060a74213f131d383f5fce473863a
mlmar <marcloussm@gmail.com>
2026-08-14 15:52:11 -0700

    Removed unused font, move About section to separate component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,5 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
     server: {
         port: 3000,
-    },
-    fonts: [
-        {
-            name: 'DM Sans',
-            cssVariable: '--font-dm-sans',
-            provider: fontProviders.google(),
-            weights: ['100 900'],
-            styles: ['normal'],
-            fallbacks: ['sans-serif']
-        }
-    ]
+    }
 });
