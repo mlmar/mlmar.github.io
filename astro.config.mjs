a890e87c229c859504fc69f498753f8d9b70646a
mlmar <marcloussm@gmail.com>
2026-09-02 20:41:02 -0700

    Update og graph tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://mlmar.github.io',
     server: {
         port: 3000,
     }
